9e2c03a6f68972211125e3fd3ae8ec9251172a2f
tyler kim <yoonsoo.kim11@gmail.com>
2023-11-27 16:11:55 -0500

    practice 1st try

---
+++ src/index.js
@@ -0,0 +1,39 @@
+
+fetch('http://127.0.0.1:3000/pups')
+.then(resp => resp.json())
+.then(dogList => {
+    dogList.forEach(dog => {
+    
+    const pupNameSpanElement = document.createElement('span')
+    const pupNameDivElement = document.getElementById('dog-bar')
+    console.log(pupNameDivElement)
+
+    pupNameSpanElement.textContent = dog.name
+    pupNameDivElement.appendChild(pupNameSpanElement)
+
+    //details
+    pupNameSpanElement.addEventListener('click', e => {
+        const pupDetailDiv = document.getElementById('dog-info')
+        const pupDetailImg = document.createElement('img')
+        const pupDetailName = document.createElement('h2')
+        const pupDetailButton = document.createElement('button')
+    
+        pupDetailImg.src = dog.image
+        pupDetailName.textContent = dog.name
+        pupDetailButton.textContent = dog.isGoodDog ? "Good Dog!" : "Bad Dog!"
+    
+        pupDetailDiv.append(pupDetailImg, pupDetailName, pupDetailButton)
+
+//good dog button
+pupDetailButton.addEventListener('click', e => {
+    dog.isGoodDog = !dog.isGoodDog
+    pupDetailButton.textContent = dog.isGoodDog ? "Good Dog!" : "Bad Dog!"
+})
+
+    })
+
+    
+})
+
+
+})
