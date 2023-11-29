1cb216e6ba7dcdc8e296b20ba74c5a1d98557609
tyler kim <yoonsoo.kim11@gmail.com>
2023-11-29 15:09:14 -0500

    completed practice with pessimistic patch

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 
 //1 create span element. set textcontenet to name of dog and append to div dogbar
 const dogBarElement = document.getElementById('dog-bar')
-
+const dogInfoElement = document.getElementById('dog-info')
+let currentDog
 fetch('http://127.0.0.1:4000/pups')
     .then(resp => resp.json())
     .then(dogArray => {
@@ -10,15 +11,45 @@ fetch('http://127.0.0.1:4000/pups')
     })
 })
 
-
 function displayDogNameinBar(singleDog){
+
     const dogSpanElement = document.createElement('span')
     dogSpanElement.textContent = singleDog.name
     dogBarElement.appendChild(dogSpanElement)
 
+    //2 click event on spanelement. create image element, h2, button, set its content and append to dog-info div
+    dogSpanElement.addEventListener('click', ()=> {
+    currentDog = singleDog
+
+        dogInfoElement.innerHTML = ''
+        const imgElement = document.createElement('img')
+        const nameELement = document.createElement('h2')
+        const buttonElement = document.createElement('button')
+
+        imgElement.src = singleDog.image
+        nameELement.textContent = singleDog.name
+        buttonElement.textContent = singleDog.isGoodDog? "Good Dog!" : " Bad Dog!"
+        dogInfoElement.append(imgElement, nameELement, buttonElement)
+
+        buttonElement.addEventListener('click', ()=> {
+            currentDog.isGoodDog = !currentDog.isGoodDog
+            
+            let updatedGoodDog = {
+                isGoodDog: currentDog.isGoodDog
+            }
+            fetch(`http://127.0.0.1:4000/pups/${currentDog.id}`, {
+                method: "PATCH",
+                headers: { "Content-Type": "application/json"},
+                body: JSON.stringify(updatedGoodDog)
+            })
+                .then(resp => resp.json())
+                .then(updatedGoodDogData => {
+                    console.log(buttonElement.textContent)
+                    buttonElement.textContent = updatedGoodDogData.isGoodDog? " Good Dog!" : "Bad Dog!"
+                })
+        })
+    })
 }
-
-
 
 
 
